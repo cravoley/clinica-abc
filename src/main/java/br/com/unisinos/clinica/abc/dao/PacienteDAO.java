ec0f65fe09907d0283ccf6fa500cbbf0e9f3b666
Valdo Raya <cravoley@gmail.com>
2016-11-19 19:47:08 -0200

    Adiciona testes relacionados ao requisito de anexar exames

---
+++ src/main/java/br/com/unisinos/clinica/abc/dao/PacienteDAO.java
@@ -0,0 +1,10 @@
+package br.com.unisinos.clinica.abc.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacienteDAO {
+	public boolean hasPrimeiraConsulta(String nomePaciente) {
+		return false;
+	}
+}
---
+++ src/test/java/br/com/unisinos/clinica/abc/test/util/BaseTest.java
@@ -0,0 +1,10 @@
+package br.com.unisinos.clinica.abc.test.util;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration(locations = "classpath*:spring-test-context.xml")
+public class BaseTest {
+}
