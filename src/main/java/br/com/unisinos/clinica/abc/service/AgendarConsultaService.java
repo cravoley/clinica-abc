3312855bca72f4d22c3746a446861ba9dbe0610c
Rômulo Leidemer <romuloleidemer@gmail.com>
2016-11-20 14:59:05 -0200

    Ajuste dos testes para uso do Mockito.

--- src/main/java/br/com/unisinos/clinica/abc/service/AgendarConsultaService.java
+++ src/main/java/br/com/unisinos/clinica/abc/service/AgendarConsultaService.java
@@ -7,166 +7,42 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.unisinos.clinica.abc.dao.HorarioIndisponivelDAO;
+import br.com.unisinos.clinica.abc.dao.ItemAgendaDAO;
 import br.com.unisinos.clinica.abc.model.tratamento.consulta.agenda.HorarioIndisponivel;
 import br.com.unisinos.clinica.abc.model.tratamento.consulta.agenda.ItemAgenda;
 
+@Service
 public class AgendarConsultaService {
+
+	@Autowired private HorarioIndisponivelDAO horarioIndisponivelDAO;
+	@Autowired private ItemAgendaDAO itemAgendaDAO;
 	
-	private static AgendarConsultaService uniqueInstance;
-	private List<ItemAgenda> itensAgendado;
-	private List<HorarioIndisponivel> horariosIndisponiveis;
-
-	private AgendarConsultaService() throws ParseException {
-		itensAgendado = new ArrayList<>();
-		horariosIndisponiveis = new ArrayList<>();
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
-		String dataInicioString = "11/11/2016 08:00:00";
-		Date dataInicio = sdf.parse(dataInicioString);
-		String dataFimString = "11/11/2016 09:00:00";
-		Date dataFim = sdf.parse(dataFimString);
-		
-		ItemAgenda itemAgenda = new ItemAgenda();
-		itemAgenda.setInicio(dataInicio);
-		itemAgenda.setFim(dataFim);
-		itemAgenda.setDescricao("Consulta João da Silva");
-		itemAgenda.setLocal("Clínica Vida");
-		
-		itensAgendado.add(itemAgenda);
-		
-		sdf = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
-		dataInicioString = "11/11/2016 09:00:00";
-		dataInicio = sdf.parse(dataInicioString);
-		dataFimString = "11/11/2016 10:00:00";
-		dataFim = sdf.parse(dataFimString);
-		
-		itemAgenda = new ItemAgenda();
-		itemAgenda.setInicio(dataInicio);
-		itemAgenda.setFim(dataFim);
-		itemAgenda.setDescricao("Consulta Alberto Carlos");
-		itemAgenda.setLocal("Clínica Vida");
-		
-		itensAgendado.add(itemAgenda);
-		
-		sdf = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
-		dataInicioString = "11/11/2016 10:00:00";
-		dataInicio = sdf.parse(dataInicioString);
-		dataFimString = "11/11/2016 11:00:00";
-		dataFim = sdf.parse(dataFimString);
-		
-		itemAgenda = new ItemAgenda();
-		itemAgenda.setInicio(dataInicio);
-		itemAgenda.setFim(dataFim);
-		itemAgenda.setDescricao("Consulta Maurício Souza");
-		itemAgenda.setLocal("Clínica Vida");
-		
-		itensAgendado.add(itemAgenda);
-		
-		sdf = new SimpleDateFormat("dd/M/yyyy hh:mm:ss");
-		dataInicioString = "11/11/2016 11:00:00";
-		dataInicio = sdf.parse(dataInicioString);
-		dataFimString = "11/11/2016 12:00:00";
-		dataFim = sdf.parse(dataFimString);
-		
-		itemAgenda = new ItemAgenda();
-		itemAgenda.setInicio(dataInicio);
-		itemAgenda.setFim(dataFim);
-		itemAgenda.setDescricao("Consulta Carlos Miguel");
-		itemAgenda.setLocal("Clínica Vida");
-		
-		itensAgendado.add(itemAgenda);
-		
-		SimpleDateFormat stf = new SimpleDateFormat("hh:mm:ss");
-		String horaInicioString = "08:00:00";
-		String horaFimString = "12:00:00";
-		Date horaInicio = stf.parse(horaInicioString);
-		Date horaFim = stf.parse(horaFimString);
-		
-		HorarioIndisponivel horarioIndisponivel = new HorarioIndisponivel();
-		horarioIndisponivel.setDiaSemana(2);
-		horarioIndisponivel.setHoraInicio(horaInicio);
-		horarioIndisponivel.setHoraFim(horaFim);
-		horarioIndisponivel.setMotivo("Convensão");
-		
-		horariosIndisponiveis.add(horarioIndisponivel);		
-	}
-
-	public static synchronized AgendarConsultaService getInstance() throws ParseException {
-		if (uniqueInstance == null)
-			uniqueInstance = new AgendarConsultaService();
-
-		return uniqueInstance;
+	private static Logger logger = LoggerFactory.getLogger(AnexarExameService.class);
+	
+	public boolean hasItemAgendaNaData(Date dataInicio, Date dataFim) {
+		return itemAgendaDAO.hasItemAgendaNaData(dataInicio, dataFim);
 	}
 	
+	public boolean hasHorarioIndisponivelNaData(Date dataInicio, Date dataFim) {
+		return horarioIndisponivelDAO.hasHorarioIndisponivelNaData(dataInicio, dataFim);
+	}	
 	
-	public ItemAgenda salvar(ItemAgenda itemAgenda) throws Exception {
-		
-		if (!validarCamposObrigatorios(itemAgenda)) {
-			throw new Exception("Campos obrigatórios devem ser preenchidos!");
-		}
-		
-		if (existeConsultaNaData(itemAgenda)) {
-			throw new Exception("Já existe uma consulta no horário "+ itemAgenda.getInicio()  +"!");
-		}
-		
-		if (horarioIndisponivel(itemAgenda)) {
-			throw new Exception("Horario indisponível "+ itemAgenda.getInicio()  +"!");
-		}
-		
-		return itemAgenda;
+	public boolean save(ItemAgenda itemAgenda) throws Exception {
+		return itemAgendaDAO.save(itemAgenda);
 	}
 	
-	private boolean validarCamposObrigatorios(ItemAgenda itemAgenda) {
+	public boolean validarCamposObrigatorios(ItemAgenda itemAgenda) {
 		boolean valido = false;
 		valido = ((itemAgenda.getInicio() != null) && (itemAgenda.getFim() != null) && 
 				(itemAgenda.getDescricao() != null)  && (itemAgenda.getLocal() != null));
 		
 		return valido;
 	}
-	
-	private boolean existeConsultaNaData(ItemAgenda itemAgenda) {
-		boolean existe = false;
-		for (ItemAgenda itemAgendaBusca : itensAgendado) {
-			existe = itemAgendaBusca.getInicio().equals(itemAgenda.getInicio());
-			if (existe) {
-				break;
-			}
-		}
 		
-		return existe;
-	}
-	
-	private boolean horarioIndisponivel(ItemAgenda itemAgenda) {
-		boolean indisponivel = false;
-		for (HorarioIndisponivel horarioIndisponivel : horariosIndisponiveis) {
-			
-			GregorianCalendar gc = new GregorianCalendar();
-            gc.setTime(itemAgenda.getInicio());
-            int diaDaSemana = gc.get(GregorianCalendar.DAY_OF_WEEK);
-            
-            if (diaDaSemana == horarioIndisponivel.getDiaSemana()) {
-            	//TODO
-            	indisponivel = true;
-            	break;
-            }
-		}
-		
-		return indisponivel;
-	}
-
-	public List<ItemAgenda> getItensAgendado() {
-		return itensAgendado;
-	}
-
-	public void setItensAgendado(List<ItemAgenda> itensAgendado) {
-		this.itensAgendado = itensAgendado;
-	}
-
-	public List<HorarioIndisponivel> getHorariosIndisponiveis() {
-		return horariosIndisponiveis;
-	}
-
-	public void setHorariosIndisponiveis(List<HorarioIndisponivel> horariosIndisponiveis) {
-		this.horariosIndisponiveis = horariosIndisponiveis;
-	}
 }
---
+++ src/test/java/br/com/unisinos/clinica/abc/service/AgendarConsultaServiceTest.java
@@ -0,0 +1,67 @@
+package br.com.unisinos.clinica.abc.service;
+
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import br.com.unisinos.clinica.abc.dao.HorarioIndisponivelDAO;
+import br.com.unisinos.clinica.abc.dao.ItemAgendaDAO;
+import br.com.unisinos.clinica.abc.model.tratamento.consulta.agenda.ItemAgenda;
+import br.com.unisinos.clinica.abc.test.util.TestBase;
+
+public class AgendarConsultaServiceTest extends TestBase {
+	
+	@Mock private HorarioIndisponivelDAO horarioIndisponivelDAO;
+	@Mock private ItemAgendaDAO itemAgendaDAO;
+
+	@InjectMocks private AgendarConsultaService service;
+	
+	private ItemAgenda itemAgenda;
+	
+	@Before
+    public void setup() {
+		itemAgenda = new ItemAgenda();
+		itemAgenda.setDescricao("Descrição consulta");
+		itemAgenda.setLocal("Clínica vida");
+		itemAgenda.setInicio(new Date());
+		itemAgenda.setFim(new Date());
+		itemAgenda.setAtivo(true);
+    }
+	
+	@Test
+	public void hasItemAgendaNaData() throws Exception {
+		Mockito.when(itemAgendaDAO.hasItemAgendaNaData(Mockito.anyObject(), Mockito.anyObject())).thenReturn(true, false);
+
+		Assert.assertTrue(service.hasItemAgendaNaData(new Date(), new Date()));
+		Assert.assertFalse(service.hasItemAgendaNaData(new Date(), new Date()));
+	}
+	
+	@Test
+	public void hasHorarioIndisponivelNaData() throws Exception {
+		Mockito.when(horarioIndisponivelDAO.hasHorarioIndisponivelNaData(Mockito.anyObject(), Mockito.anyObject())).thenReturn(true, false);
+
+		Assert.assertTrue(service.hasHorarioIndisponivelNaData(new Date(), new Date()));
+		Assert.assertFalse(service.hasHorarioIndisponivelNaData(new Date(), new Date()));
+	}
+	
+	@Test
+	public void validarCamposObrigatorios() throws Exception {		
+		Assert.assertTrue(service.validarCamposObrigatorios(itemAgenda));
+		ItemAgenda itemAgendaInvalido = new ItemAgenda();
+		Assert.assertFalse(service.validarCamposObrigatorios(itemAgendaInvalido));
+	}
+	
+	@Test
+	public void save() throws Exception {		
+		Mockito.when(itemAgendaDAO.save(itemAgenda)).thenReturn(true);
+		
+		Assert.assertTrue(service.save(itemAgenda));
+	}
+
+}
